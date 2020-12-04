6487ec8ee549e4a7906d7f209bd430ba3fa7fab1
developer-cross <66529240+developer-cross@users.noreply.github.com>
2020-12-04 18:48:48 -0300

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import React from "react";
+import Navbar from "./Components/Navbar/index";
+
+export default function App() {
+  return (
+    <div className="App">
+      <Navbar />
+    </div>
+  );
+}
